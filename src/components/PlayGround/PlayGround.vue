a3c197e254f43d878880f869b0f39042b161eb1f
Lu Xie <xiemegan447@gmail.com>
2019-01-17 11:59:36 -0500

    initializing playground with sepration of concern

---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <button>click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayGround",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src='./PlayGround/PlayGround.css' scoped>
+</style>
